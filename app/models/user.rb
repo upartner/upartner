9e2b43952ab888607330f54d11672b6a160d407e
yamadatakashi <heavymetalshout@gmail.com>
2013-10-26 14:32:33 +0900

    users/new　の新規会員登録フォームのメール、その他の正規表現などのバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+     VALID_MAIL_REGEX = /\A[0-9A-Za-z_-]+@([0-9a-z-]+\.)+[0-9a-z-]+\z/
+     VALID_REGEX = /\A[0-9A-Za-z_-]+\z/
+
+     validates :user_id,   :presence => true, format:VALID_REGEX
+     validates :name,  :presence => true
+     validates :email, presence: true, :confirmation => true, format:VALID_MAIL_REGEX
+     validates :password,  :presence => true, :confirmation => true, format:VALID_REGEX
 end
